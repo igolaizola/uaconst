54eee73619dc387f51dd215d9d0ec62f2e1ea5f7
igolaizola <11333576+igolaizola@users.noreply.github.com>
2025-07-30 23:07:59 +0200

    Added headers returned after Accept-CH

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ import "github.com/igolaizola/uaconst"
 req.Header.Set("User-Agent", uaconst.DefaultUserAgent)
 req.Header.Set("sec-ch-ua", uaconst.DefaultSecChUa)
 ```
+
+## Update constants
+
+Open https://httpbin.org/response-headers?Accept-CH=Sec-CH-UA-Full-Version-List%2CSec-CH-UA-Platform-Version in your browser and copy the `sec-ch-ua` headers and `user-agent` header to the `uaconst` package.
--- uaconst.go
+++ uaconst.go
@@ -1,9 +1,13 @@
 package uaconst
 
 const (
-	DefaultUserAgent = Chrome138UserAgent
-	DefaultSecChUa   = Chrome138SecChUa
+	DefaultUserAgent              = Chrome138UserAgent
+	DefaultSecChUa                = Chrome138SecChUa
+	DefaultSecChUaFullVersionList = Chrome138SecChUaFullVersionList
+	DefaultSecChUaPlatformVersion = Chrome138SecChUaPlatformVersion
 
-	Chrome138SecChUa   = `"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"`
-	Chrome138UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+	Chrome138UserAgent              = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"
+	Chrome138SecChUa                = `"Not)A;Brand";v="8", "Chromium";v="138", "Google Chrome";v="138"`
+	Chrome138SecChUaFullVersionList = `"Not)A;Brand";v="8.0.0.0", "Chromium";v="138.0.7204.169", "Google Chrome";v="138.0.7204.169"`
+	Chrome138SecChUaPlatformVersion = `"19.0.0"`
 )
